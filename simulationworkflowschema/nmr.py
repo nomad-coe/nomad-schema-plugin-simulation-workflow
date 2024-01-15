e15555fc88e127941c6a24fbfd2234f6e14d9ff2
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-01-15 16:00:51 +0100

    Deleted unused imports

--- simulationworkflowschema/nmr.py
+++ simulationworkflowschema/nmr.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from nomad.metainfo import SubSection, Quantity, Reference
+from nomad.metainfo import SubSection
 from .general import (
     DFTOutputs,
     NMROutputs,
