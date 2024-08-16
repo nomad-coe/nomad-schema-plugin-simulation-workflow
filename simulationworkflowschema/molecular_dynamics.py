883708838ffaad764d8258fc345ce702edd09234
jrudz <rudzinski@mpip-mainz.mpg.de>
2024-08-16 14:05:40 +0200

    fixed rg issue

--- simulationworkflowschema/molecular_dynamics.py
+++ simulationworkflowschema/molecular_dynamics.py
@@ -2203,7 +2203,7 @@ class RadiusOfGyration(TrajectoryProperty):
 
     atomsgroup_ref = Quantity(
         type=Reference(AtomsGroup.m_def),
-        shape=[1],
+        shape=[],
         description="""
         References to the atoms_group section containing the molecule for which Rg was calculated.
         """,
